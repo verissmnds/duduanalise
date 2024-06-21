1b3c71eb38707082118e4b4d099f0624f1469d14
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2024-06-21 18:35:28 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_excel('posts1.xlsx')
+
+dudu_df = df[df['Profile'] == 'Eduardo Paes']
+
+st.set_page_config(page_title="Análise do Instagram de Eduardo Paes", page_icon="📐", layout="wide")
+
+st.title('Análise do Instagram de Eduardo Paes')
+
+df2 = dudu_df.sort_values(by='Post interaction rate', ascending=False).head(10)
+
+st.header("Nuvem de palavras das legendas escritas pelo pré-candidato em suas publicações:")
+
+url_imagem = 'fotodudu.png'
+st.image(url_imagem)
+
+st.header("As 10 publicações com maior taxa de engajamento:")
+st.markdown("Na análise eu foquei apenas nas três primeiras publicações:")
+st.markdown("1° https://www.instagram.com/reel/C1UQXF1OSRc/")
+st.markdown("2° https://www.instagram.com/reel/C16_3gcuNm4/")
+st.markdown("3° https://www.instagram.com/reel/C0T7jIgOuB_/")
+
+st.dataframe(df2)
